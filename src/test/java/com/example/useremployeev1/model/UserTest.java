de557f0f735543261c089ef026473ee2c0b55432
CodeClod <mika9103@stud.kea.dk>
2023-02-07 14:15:09 +0100

    Added: - small J-unit test

--- src/test/java/com/example/useremployeev1/model/UserTest.java
+++ src/test/java/com/example/useremployeev1/model/UserTest.java
@@ -1,5 +1,8 @@
 package com.example.useremployeev1.model;
 
+import com.example.useremployeev1.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class UserTest {
 
-
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Test
+    void testGin() {
+        assertEquals(true,employeeRepository.findByName("Gin").isPresent());
+    }
 }
